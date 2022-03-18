c7c5112761829bdd7bb987704c7b158e3a0aa3c4
JanSzalanski <jan@szalanski.eu>
2022-03-18 19:47:55 +0100

    added more improvments to project

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
+const camera = document.querySelector(".camera");
+const Btn1 = document.queryCommandValue(".buttonA");
+const Btn2 = document.queryCommandValue(".buttonB");
+let root = document.documentElement;
+let cameraH = "2";
+camera.style.setProperty("perspective-origin", `50% calc(50% - ${cameraH}em`);
+output.innerHTML = 20;
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
   output.innerHTML = this.value;
+  cameraH = this.value / 10;
+  camera.style.setProperty("perspective-origin", `50% calc(50% - ${cameraH}em`);
 };
