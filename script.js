eb57ef25e7b59226e589550a9adb6dc3b31eabc3
JanSzalanski <jan@szalanski.eu>
2022-03-18 22:49:54 +0100

    final version to publish

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 const camera = document.querySelector(".camera");
-const Btn1 = document.queryCommandValue(".buttonA");
-const Btn2 = document.queryCommandValue(".buttonB");
+const Btn1 = document.querySelector(".buttonA");
+const Btn2 = document.querySelector(".buttonB");
+const Cube2 = document.querySelector(".cube2");
 let root = document.documentElement;
 let cameraH = "2";
 camera.style.setProperty("perspective-origin", `50% calc(50% - ${cameraH}em`);
@@ -15,3 +16,13 @@ slider.oninput = function () {
   cameraH = this.value / 10;
   camera.style.setProperty("perspective-origin", `50% calc(50% - ${cameraH}em`);
 };
+
+const BoxAnimation = () => {
+  Cube2.classList.toggle("animation");
+};
+
+const BoxVisibility = () => {
+  Cube2.classList.toggle("visibilty");
+};
+Btn1.addEventListener("click", BoxVisibility);
+Btn2.addEventListener("click", BoxAnimation);
